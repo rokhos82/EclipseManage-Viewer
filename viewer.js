022f0a8b3469d2a0e8be51e73cb74af57bcaee70
Justin Lane <justin.l.lane@gmail.com>
2018-05-15 21:32:50 -0600

    Parse Data
    Groups and projects

--- viewer.js
+++ viewer.js
@@ -21,8 +21,25 @@
 
         activate();
 
+        $ctrl.filterProjects = filterProjects;
+
         function activate() {
           $ctrl.data = viewerData;
+          $log.info(viewerData);
+
+          $ctrl.groups = $ctrl.data[0];
+          $ctrl.projects = $ctrl.data[1];
+          $ctrl.tasks = $ctrl.data[2];
+        }
+
+        function filterProjects(groupId) {
+          var result = {};
+          angular.forEach($ctrl.projects,function(project,id) {
+            if(project.hasOwnProperty('y') && project.y === groupId) {
+              result[id] = project;
+            }
+          });
+          return result;
         }
     }
 })();
